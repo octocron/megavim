976ebe98706a8ecff94f691b3787ece33712a072
megacron <megacron@d3c3p7.com>
2024-01-10 20:17:39 -0500

    lazy vim w/colorscheme

---
+++ init.lua
@@ -0,0 +1,35 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+local plugins = {
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+        -- custom options here
+        priority = 1000,
+    },
+    config = function(_, opts)
+        require("tokyodark").setup(opts) -- calling setup is optional
+        vim.cmd [[colorscheme tokyodark]]
+    end,
+  }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
