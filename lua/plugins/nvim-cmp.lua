8beacda39002acdfde78b68a8f62252194928eb0
megacron <megacron@d3c3p7.com>
2024-01-11 20:16:07 -0500

    lspkind -converted words to symbols, move global leader back to init.lua

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-cmdline",
     "L3MON4D3/LuaSnip", -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
@@ -14,6 +15,20 @@ return {
     local cmp = require("cmp")
 
     local luasnip = require("luasnip")
+
+    local lspkind = require('lspkind')
+    cmp.setup {
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol', -- show only symbol annotations
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                         -- can also be a function to dynamically calculate max width such as 
+                         -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+        })
+      }
+    }
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
