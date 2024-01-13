e76ea6612fefc373ff766f0e5634177e6d83bbd3
megacron <megacron@d3c3p7.com>
2024-01-12 23:18:41 -0500

    none-ls formatting and liting go, lua, javascript etc

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,18 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.goimports, -- go
+				null_ls.builtins.formatting.stylua, -- lua
+				null_ls.builtins.formatting.prettier, -- CSS HTML JSON YAML MARKDOWN SCSS
+				null_ls.builtins.diagnostics.ansible-lint,  -- ansible
+				null_ls.builtins.diagnostics.eslint_d,  -- javascript
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
