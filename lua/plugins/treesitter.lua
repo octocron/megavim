fd8253115fd11ceaa87bcf56a469bb94fc4599c2
megacron <megacron@d3c3p7.com>
2024-02-07 21:44:28 -0500

    autotag -> treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,38 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      ignore_install = {"cobol"},
-      sync_install = false,
-      ensure_installed = {
-        "bash",
-        "css",
-        "dockerfile",
-        "c",
-        "go",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "nix",
-        "python",
-        "regex",
-        "rust",
-        "terraform",
-        "toml",
-        "query",
-        "yaml",
-        "vim",
-        "vimdoc"
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			ignore_install = { "cobol" },
+			sync_install = false,
+			ensure_installed = {
+				"bash",
+				"css",
+				"dockerfile",
+				"c",
+				"go",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"nix",
+				"python",
+				"regex",
+				"rust",
+				"terraform",
+				"toml",
+				"query",
+				"yaml",
+				"vim",
+				"vimdoc",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			autotag = { enable = true },
+		})
+	end,
 }
