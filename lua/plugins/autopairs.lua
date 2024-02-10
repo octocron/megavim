b6c3968823b5ff46b691d19ec5ef49c0b786fde9
megacron <megacron@d3c3p7.com>
2024-02-09 21:08:02 -0500

    tried to add < pair cmp

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -16,6 +16,18 @@ return {
 				javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
 				java = false, -- don't check treesitter on java
 			},
+			disable_filetype = { "TelescopePrompt", "spectre_panel" },
+			fast_wrap = {
+				map = "<M-e>",
+				chars = { "{", "[", "(", "<", '"', "'" },
+				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+				offset = 0, -- Offset from pattern match
+				end_key = "$",
+				keys = "qwertyuiopzxcvbnmasdfghjkl",
+				check_comma = true,
+				highlight = "PmenuSel",
+				highlight_grey = "LineNr",
+			},
 		})
 
 		-- import nvim-autopairs completion functionality
