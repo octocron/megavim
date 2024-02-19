81de72eef461ab743b06325fa9b23572e9565523
megacron <megacron@d3c3p7.com>
2024-02-19 01:21:09 -0500

    harpoon2 not pulling, since it keeps using Capitalized branch instead of lower

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,20 +1,32 @@
 --------------------------------------------------------------------------------
 -- TODO Comments
 --------------------------------------------------------------------------------
+-- INFO: Install rg;
 return {
   {
     -- TODO: makes comments pop!
     -- FIX: this needs fixing
     -- HACK: this seems janky
-    -- INFO: information
-    -- NOTE: also informational
     -- WARN: give warning about something
+    -- NOTE: also informational
     -- PERF: performance or optimise related
     -- TEST: testing pass | fail
     "folke/todo-comments.nvim",
     event = "BufRead",
+    keys = {
+      { "<leader>tl", "<cmd>TodoLocList<cr>", desc = "Local TODOs List" },
+      { "<leader>tq", "<cmd>TodoQuickFix<cr>", desc = "TODOs Quick Fix" },
+      { "<leader>ts", "<cmd>TodoTrouble<cr>", desc = "List TODOs with Trouble" },
+      { "<leader>tt", "<cmd>TodoTelescope<cr>", desc = "List TODOs with Telescope" },
+    },
     config = function()
-      require("todo-comments").setup()
+      require("todo-comments").setup({
+        colors = {
+          error = { "DiagnosticError", "ErrorMsg", "#880022" },
+          test = { "Identifier", "#228800" },
+          info = { "DiagnosticInfo", "#2563eb" },
+        },
+      })
     end,
   },
 }
