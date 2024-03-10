7b0496d18741c2f3d449ff874a45918f83b8e992
megacron <megacron@d3c3p7.com>
2024-03-10 17:17:55 -0400

    bg for notify, corrected nix autoarchive to work with nil

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -5,14 +5,16 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
     },
-    require("aerial").setup({
-      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-      on_attach = function(bufnr)
-        -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "<leader>aa", "<cmd>AerialToggle!<CR>", { desc = "aerial toggle" })
-      end,
-    }),
+    config = function()
+      require("aerial").setup({
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "<leader>aa", "<cmd>AerialToggle!<CR>", { desc = "aerial toggle" })
+        end,
+      })
+    end,
   },
 }
