4ee8937c04851d4393c77ed1edc47efaeccedcac
megacron <megacron@d3c3p7.com>
2024-03-10 15:34:13 -0400

    changes to lsp-conf

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,78 +2,85 @@
 -- Mason
 --------------------------------------------------------------------------------
 return {
-  -- INFO:Installs Mason
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  -- INFO: bridge for mason with lspconfig plugin by installing servers on your system
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  -- INFO: the actual lsp plugin
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	-- INFO:Installs Mason
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	-- INFO: bridge for mason with lspconfig plugin by installing servers on your system
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	-- INFO: the actual lsp plugin
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-      lspconfig.golangci_lint_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.nil_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.taplo.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        settings = {
-          Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using
-              -- (most likely LuaJIT in the case of Neovim)
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = {
-                "vim",
-                "require",
-              },
-            },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "lsp buffer hover" })
-      vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "lsp definition" })
-      vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, { desc = "lsp implementation" })
-      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "lsp references" })
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "code action" })
-    end,
-  },
+			lspconfig.golangci_lint_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.nil_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.nix_lsp.setup({
+				settings = {
+					nix = {
+						autoArchive = true,
+					},
+				},
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.taplo.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = {
+								"vim",
+								"require",
+							},
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						-- Do not send telemetry data containing a randomized but unique identifier
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "lsp buffer hover" })
+			vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "lsp definition" })
+			vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, { desc = "lsp implementation" })
+			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "lsp references" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "code action" })
+		end,
+	},
 }
