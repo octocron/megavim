96b2a2992072e16c3cb98df0dd20819211888832
megacron <megacron@d3c3p7.com>
2024-03-13 20:32:26 -0400

    fixed luasnip @ rm -rf ~/.local/share/nvim/lazy/LuaSnip

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,19 @@ vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+
+--globally add syntax highlighting
+vim.cmd('syntax on')
+
+-- Enable filetype detection
+vim.cmd('filetype on')
+vim.cmd('filetype plugin indent on')
+
+-- Add custom filetype detection for .rasi files
+vim.cmd('autocmd BufRead,BufNewFile *.rasi set filetype=rasi')
+vim.cmd('autocmd BufRead,BufNewFile *.rasi set syntax=rasi')
+
+-- Define autocmds to set the filetype and enable syntax highlighting for .conf files
+vim.cmd('autocmd BufRead,BufNewFile *.conf set filetype=conf')
+vim.cmd('autocmd BufRead,BufNewFile *.conf set syntax=conf')
+
