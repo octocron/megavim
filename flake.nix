3e45b0178e5c7249a5fc20a8321d12c58423a6ec
megacron <megacron@d3c3p7.com>
2024-03-17 18:08:39 -0400

    concerned to manage many hashii for nvim

--- flake.nix
+++ flake.nix
@@ -1,62 +1,48 @@
 {
-  description = "NeoVim via Nix";
+  description = "megavim";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "gitlab:megacron/megavim/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    tree-sitter-nu = {
-      url = "github:nushell/tree-sitter-nu";
-      flake = false;
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, nixvim, flake-parts, tree-sitter-nu, pre-commit-hooks
-    , ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems =
-        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
 
-      perSystem = { system, pkgs, self', lib, ... }:
-        let
-          nixvim' = nixvim.legacyPackages.${system};
-          nvim = nixvim'.makeNixvimWithModule {
-            inherit pkgs;
-            module = ./config;
-          };
-        in {
-          checks = {
-            default = pkgs.nixvimLib.check.mkTestDerivationFromNvim {
-              inherit nvim;
-              name = "A nixvim configuration";
-            };
-            pre-commit-check = pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                statix.enable = true;
-                nixfmt.enable = true;
-              };
-            };
-          };
+      src = ./.;
 
-          formatter = pkgs.nixfmt;
+      snowfall = {
+        namespace = "plusultra";
+      };
 
-          packages = rec {
-            default = full;
-            full = nvim;
-          };
+      channels-config.allowUnfree = true;
 
-          devShells = {
-            default = with pkgs;
-              mkShell { inherit (self'.checks.pre-commit-check) shellHook; };
+      alias.packages.default = "neovim";
+
+      outputs-builder = channels: {
+        formatter = channels.nixpkgs.alejandra;
+
+        checks.pre-commit-check = inputs.pre-commit-hooks.lib.${channels.nixpkgs.system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
           };
         };
+      };
     };
 }
