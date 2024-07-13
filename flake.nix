39fd444e9e436b39914f031f48e3db787e1f61f1
megacron <megacron@d3c3p7.com>
2024-07-13 01:52:18 -0400

    prelim working standalone nixvim

--- flake.nix
+++ flake.nix
@@ -1,48 +1,72 @@
 {
-  description = "megavim";
+  description = "megavim - a nixvim standalone package";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     nixvim = {
       url = "gitlab:megacron/megavim/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs:
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
+  outputs = { self, flake-parts, nixpkgs, nixvim, pre-commit-hooks, ... } @ inputs:
+    let
+      config = import ./config; # import the module directly
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      imports = [ inputs.devenv.flakeModule ];
 
-      src = ./.;
+      perSystem = { pkgs, system, ... }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nvim = nixvim'.makeNixvimWithModule {
+            inherit pkgs;
+            module = config;
+          };
+        in
+        {
+          checks = {
+            # Run `nix flake check .` to verify that your config is not broken
+            default = nixvimLib.check.mkTestDerivationFromNvim {
+              inherit nvim;
+              name = "megavim";
+            };
+          };
 
-      snowfall = {
-        namespace = "plusultra";
-      };
+          packages = {
+            # Lets you run `nix run .` to start nixvim
+            default = nvim;
+          };
 
-      channels-config.allowUnfree = true;
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+            };
+          };
 
-      alias.packages.default = "neovim";
-
-      outputs-builder = channels: {
-        formatter = channels.nixpkgs.alejandra;
-
-        checks.pre-commit-check = inputs.pre-commit-hooks.lib.${channels.nixpkgs.system}.run {
-          src = ./.;
-          hooks = {
-            alejandra.enable = true;
+          devShells = {
+            default = pkgs.mkShell {
+              inherit (self.checks.pre-commit-check) shellHook;
+              buildInputs = with pkgs; [
+                alejandra
+              ];
+            };
           };
         };
-      };
     };
 }
