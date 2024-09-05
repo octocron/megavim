d8e69e893dd2ee9a086eeeff15536c8dbeca20a8
megacron <megacron@d3c3p7.com>
2024-09-04 21:42:28 -0400

    bufferline fix

--- config/tabline/bufferline.nix
+++ config/tabline/bufferline.nix
@@ -17,38 +17,38 @@
         ];
       };
     };
-
-    keymaps = [
-      {
-        mode = "n";
-        key = "<A-l>";
-        action = "<Cmd>BufferLineCycleNext<CR>";
-        options.desc = "Next Buffer";
-      }
-      {
-        mode = "n";
-        key = "<A-h>";
-        action = "<Cmd>BufferLineCyclePrev<CR>";
-        options.desc = "Previous Buffer";
-      }
-      {
-        mode = "n";
-        key = "<leader>bp";
-        action = ":BufferLinePick<cr>";
-        options = {
-          desc = "Pick Buffer";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bse";
-        action = ":BufferLineSortByExtension<cr>";
-        options = {
-          desc = "Sort By Extension";
-          silent = true;
-        };
-      }
-    ];
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<A-l>";
+      action = "<Cmd>BufferLineCycleNext<CR>";
+      options.desc = "Next Buffer";
+    }
+    {
+      mode = "n";
+      key = "<A-h>";
+      action = "<Cmd>BufferLineCyclePrev<CR>";
+      options.desc = "Previous Buffer";
+    }
+    {
+      mode = "n";
+      key = "<leader>bp";
+      action = ":BufferLinePick<cr>";
+      options = {
+        desc = "Pick Buffer";
+        silent = true;
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>bse";
+      action = ":BufferLineSortByExtension<cr>";
+      options = {
+        desc = "Sort By Extension";
+        silent = true;
+      };
+    }
+  ];
 }
