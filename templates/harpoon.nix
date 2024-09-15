e985ba34b02d7ad7941bb29a7ea0fe4f9c212f09
megacron <megacron@d3c3p7.com>
2024-09-15 01:59:37 -0400

    harpoon doubles buffers, not sure what to disable for it to work

---
+++ templates/harpoon.nix
@@ -0,0 +1,19 @@
+{
+  plugins.harpoon = {
+    enable = true;
+    enableTelescope = true;
+    keymapsSilent = true;
+    keymaps = {
+      addFile = "<leader>ha";
+      toggleQuickMenu = "<leader>hm";
+      navNext = "<leader>hn";
+      navPrev = "<leader>hp";
+      navFile = {
+        "1" = "<leader>hj";
+        "2" = "<leader>hk";
+        "3" = "<leader>hl";
+        "4" = "<leader>h;";
+      };
+    };
+  };
+}
