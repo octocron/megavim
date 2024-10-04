b14eb1ed406eaa3114975747f078aed30d9774bb
megacron <megacron@d3c3p7.com>
2024-10-04 14:51:57 -0400

    illuminate only undrline, wont highlight text

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,13 +4,9 @@
 return {
   {
     "tiagovla/tokyodark.nvim",
-    opts = {
-        -- custom options here
-        priority = 1000,
-    },
-    config = function(_, opts)
-        require("tokyodark").setup(opts) -- calling setup is optional
-        vim.cmd [[colorscheme tokyodark]]
+    config = function()
+      require("tokyodark").setup() -- calling setup is optional
+      vim.cmd([[colorscheme tokyodark]])
     end,
-  }
+  },
 }
--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -4,17 +4,14 @@
 return {
   "RRethy/vim-illuminate",
   config = function()
-    require("illuminate").setup({
-      delay = 200,
+    require("illuminate").configure({
+      delay = 100,
       filetypes_denylist = {
         "alpha",
         "harpoon",
-        "toggleterm",
-        "NvimTree",
         "TelescopePrompt",
-        "TelescopeResults",
       },
-      min_count_to_highlight = 1,
+      min_count_to_highlight = 2,
       providers = {
         "lsp",
         "treesitter",
@@ -22,5 +19,8 @@ return {
       },
       under_cursor = true,
     })
+    vim.cmd([[hi def IlluminatedWordText cterm=underline gui=undercurl]])
+    vim.cmd([[hi def IlluminatedWordRead  cterm=underline gui=undercurl]])
+    vim.cmd([[hi def IlluminatedWordWrite cterm=underline gui=undercurl]])
   end,
 }
