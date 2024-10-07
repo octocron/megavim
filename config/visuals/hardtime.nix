0b3bd9a848df6263c2e580eac042ddbe02d10e16
megacron <megacron@d3c3p7.com>
2024-10-07 03:51:15 -0400

    seems hardtime is fully working now

--- config/visuals/hardtime.nix
+++ config/visuals/hardtime.nix
@@ -10,56 +10,20 @@
       max_time = 1000;
       notification = true; # show messages for restricted and disabled keys
       restriction_mode = "hint";
-      #restricted_keys = {
-      #  "h" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "j" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "k" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "l" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "-" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "+" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "gj" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "gk" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "<CR>" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "<C-M>" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "<C-N>" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #  "<C-P>" = [
-      #    "n"
-      #    "x"
-      #  ];
-      #};
+      restricted_keys = {
+        "h" = [ "n" "x" ];
+        "j" = [ "n" "x" ];
+        "k" = [ "n" "x" ];
+        "l" = [ "n" "x" ];
+        "-" = [ "n" "x" ];
+        "+" = [ "n" "x" ];
+        "gj" = [ "n" "x" ];
+        "gk" = [ "n" "x" ];
+        "<CR>" = [ "n" "x" ];
+        "<C-M>" = [ "n" "x" ];
+        "<C-N>" = [ "n" "x" ];
+        "<C-P>" = [ "n" "x" ];
+      };
     };
   };
 }
