af0bd655c8aaca2a17da7ca6e22a2f101a69e26d
megacron <megacron@d3c3p7.com>
2024-10-25 03:01:22 -0400

    lspsaga, fmt is alejandra, ibl has colors

--- config/visuals/indent-blankline.nix
+++ config/visuals/indent-blankline.nix
@@ -8,6 +8,9 @@
           "prompt"
           "quickfix"
           "terminal"
+          "lsp"
+          "lspinfo"
+          "nvim-tree"
         ];
         filetypes = [
           ""
@@ -15,6 +18,7 @@
           "help"
           "lspinfo"
           "man"
+          "NvimTree"
           "TelescopePrompt"
           "TelescopeResults"
           "ToggleTerm"
@@ -26,7 +30,7 @@
       };
       indent = {
         smart_indent_cap = true;
-        char = "▎";
+        char = "│";
       };
       scope = {
         enabled = true;
@@ -35,4 +39,28 @@
       };
     };
   };
+
+  extraConfigLua = ''
+        local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+    }
+
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    end)
+    require("ibl").setup { indent = { highlight = highlight } }
+  '';
 }
--- config/visuals/theme/tokyonight.nix
+++ config/visuals/theme/tokyonight.nix
@@ -1,42 +1,61 @@
-{ ... }: {
+_: {
   colorschemes = {
     tokyonight = {
       enable = true;
       settings = {
+        sidebars = [
+          "help"
+          "NvimTree"
+          "terminal"
+        ];
         style = "night";
+        styles = {
+          floats = "dark";
+          sidebars = "dark";
+          comments.italic = true;
+          keywords.italic = true;
+        };
         transparent = true;
-        onHighlights = ''
-          function(hl, c)
-              local prompt = "#2d3149"
-              hl.TelescopeNormal = {
-                  bg = c.bg_dark,
-                  fg = c.fg_dark,
-              }
-              hl.TelescopeBorder = {
-                  bg = c.bg_dark,
-                  fg = c.bg_dark,
-              }
-              hl.TelescopePromptNormal = {
-                  bg = prompt,
-              }
-              hl.TelescopePromptBorder = {
-                  bg = prompt,
-                  fg = prompt,
-              }
-              hl.TelescopePromptTitle = {
-                  bg = prompt,
-                  fg = prompt,
-              }
-              hl.TelescopePreviewTitle = {
-                  bg = c.bg_dark,
-                  fg = c.bg_dark,
-              }
-              hl.TelescopeResultsTitle = {
-                  bg = c.bg_dark,
-                  fg = c.bg_dark,
-              }
-              end
-        '';
+        terminal_colors = true;
+        #onHighlights = # INFO: lua
+        #  ''
+        #    function(hl, c)
+        #        local prompt = "#161616"
+        #        hl.BufferLineIndicatorSelected = {
+        #            fg = c.cyan,
+        #        }
+        #        hl.TelescopeNormal = {
+        #            bg = c.bg_dark,
+        #            fg = c.fg_dark,
+        #        }
+        #        hl.TelescopeBorder = {
+        #            bg = c.bg_dark,
+        #            fg = c.bg_dark,
+        #        }
+        #        hl.TelescopePromptNormal = {
+        #            bg = prompt,
+        #        }
+        #        hl.TelescopePromptBorder = {
+        #            bg = prompt,
+        #            fg = prompt,
+        #        }
+        #        hl.TelescopePromptTitle = {
+        #            bg = prompt,
+        #            fg = prompt,
+        #        }
+        #        hl.TelescopePreviewTitle = {
+        #            bg = c.bg_dark,
+        #            fg = c.bg_dark,
+        #        }
+        #        hl.TelescopeResultsTitle = {
+        #            bg = c.bg_dark,
+        #            fg = c.bg_dark,
+        #        }
+        #        hl.WhichKey = {
+        #            bg = prompt,
+        #        }
+        #        end
+        #  '';
       };
     };
   };
